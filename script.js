475cfd18fe4791eafdf0a590d199f728afed44db
Nishaak27 <159604210+Nishaak27@users.noreply.github.com>
2024-02-15 23:32:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,9 +24,16 @@ class Particle {
     gradient.addColorStop(1, `rgba(255, 255, 255, 0)`);
     
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.moveTo(this.x, this.y - this.size * 2);
+    for (let i = 0; i < 5; i++) {
+      ctx.rotate((Math.PI / 180) * 144);
+      ctx.lineTo(this.x, this.y - this.size);
+      ctx.rotate((Math.PI / 180) * 144);
+      ctx.lineTo(this.x, this.y - this.size * 2);
+    }
     ctx.fillStyle = this.color;
     ctx.fill();
+    ctx.closePath();
   }
 
   update() {
@@ -37,14 +44,9 @@ class Particle {
 }
 
 function init() {
-  const heartSize = 100;
-  const scale = 0.03;
-  const offsetX = canvas.width / 2;
-  const offsetY = canvas.height / 2 - heartSize * scale;
-
-  for (let angle = 0; angle <= Math.PI * 2; angle += 0.01) {
-    const x = heartSize * (16 * Math.pow(Math.sin(angle), 3)) * scale + offsetX;
-    const y = -heartSize * (13 * Math.cos(angle) - 5 * Math.cos(2 * angle) - 2 * Math.cos(3 * angle) - Math.cos(4 * angle)) * scale + offsetY;
+  for (let i = 0; i < 200; i++) {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
     particles.push(new Particle(x, y));
   }
 }
@@ -60,4 +62,3 @@ function animate() {
 
 init();
 animate();
-                            
